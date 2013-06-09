b91553e8f41c103363509e9b0f6f71c3d7947eeb
Jerome Belleman <jerome.belleman@gmail.com>
2013-06-09 19:18:26 +0200

    Add configuration and features

---
+++ focsink.lua
@@ -0,0 +1,50 @@
+--[[
+-- Focus sink, a simple-minded focus stealing prevention
+--]]
+
+local NAME = 'focsink'
+local ATTR = 'sink'
+
+local oldname
+
+function anythingbuturxvt(prop, cwin, id)
+    if id.class ~= 'URxvt' then
+        return true
+    end
+end
+
+defwinprop{
+    target = NAME,
+    match = anythingbuturxvt,
+}
+
+function focsink_toggle() -- FIXME Not global please, use pkg and all
+    -- Unset previous focus sink if any
+    local oldfrm
+    ioncore.region_i(function(f)
+        if f:name() == NAME then
+            oldfrm = f
+        end
+        return true
+    end, 'WFrame')
+    if oldfrm then
+        oldfrm:set_name(oldname)
+        oldfrm:set_grattr(ATTR, 'unset')
+    end
+
+    -- Set new focus sink if previous one isn't the same
+    cur = ioncore.current()
+    if obj_is(cur, 'WFrame') then
+        oldname = cur:name()
+        if cur ~= oldfrm then
+            cur:set_name(NAME)
+            cur:set_grattr(ATTR, 'set')
+        end
+    elseif obj_is(cur, 'WClientWin') then
+        oldname = cur:parent():name()
+        if cur:parent() ~= oldfrm then
+            cur:parent():set_name(NAME)
+            cur:parent():set_grattr(ATTR, 'set')
+        end
+    end
+end
